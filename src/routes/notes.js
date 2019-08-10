b2adca0ffa1481d9f4f0a7b415416025776e2a6b
micakce <micakce@gmail.com>
2019-08-10 19:06:07 -0300

    Finished CRUD Functionality

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -29,6 +29,22 @@ router.post('/notes/new-note', async (req, res) => {
     }
 });
 
+router.get('/notes/edit/:id', async (req, res) => {
+    const note = await Note.findById(req.params.id)
+    res.render('notes/edit-note', { note });
+});
+
+router.put('/notes/edit-note/:id', async (req, res) => {
+    const {title, description} = req.body;
+    await Note.findByIdAndUpdate(req.params.id, {title, description});
+    res.redirect('/notes');
+});
+
+router.delete('/notes/delete/:id', async (req, res) => {
+    await Note.findByIdAndDelete(req.params.id)
+    res.redirect('/notes')
+});
+
 router.get('/notes', async (req, res) => {
     const notes = await Note.find().sort({date: 'desc'});
     res.render('notes/all-notes', { notes });
